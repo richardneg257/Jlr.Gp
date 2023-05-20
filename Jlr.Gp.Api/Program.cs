f60f6ab3c9f779b4ce58335ee22f930fb952ae64
Richard Negron <richard.negron.257@gmail.com>
2023-05-20 01:01:56 -0500

    appsettings and cors

--- Jlr.Gp.Api/Program.cs
+++ Jlr.Gp.Api/Program.cs
@@ -19,11 +19,13 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
+
+app.UseCors("Open");
 
 app.UseAuthorization();
 
